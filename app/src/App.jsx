030a7ad382afb1401cecab02c7ef4a914622d0a2
Nungo <nued65@gmail.com>
2026-06-26 04:41:54 +0300

    fix:move pics needed by json to public directory as they are not handled by vite unlike in src hence fixing images linked to json

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,7 +5,6 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Button from './components/Button'
 import Card from './components/Card'
-import Picture from './assets/groupie.png'
 
 function App() {
   const [count, setCount] = useState(0)
